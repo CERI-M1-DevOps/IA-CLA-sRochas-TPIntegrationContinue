4b1dd4af7b3e2545f305432b5cce0d0ec54c1e83
ahrizmo <uapv2401091@e-mc129-16.ad.univ-avignon.fr>
2026-09-17 18:44:14 +0200

    tp1-exo3-2

--- src/test/java/liste/ListeSimpleTest.java
+++ src/test/java/liste/ListeSimpleTest.java
@@ -228,10 +228,9 @@ public class ListeSimpleTest {
         listeATester.echanger(r1, r2);
         assertEquals("ListeSimple(Noeud(1), Noeud(4), Noeud(3), Noeud(2), Noeud(5))",listeATester.toString());
         r1 = listeATester.tete;
-        listeATester.ajout(1);
         r2 = listeATester.tete;
         listeATester.echanger(r1, r2);
-        assertEquals("ListeSimple(Noeud(1), Noeud(1), Noeud(4), Noeud(3), Noeud(2), Noeud(5))",listeATester.toString());
+        assertEquals("ListeSimple(Noeud(1), Noeud(4), Noeud(3), Noeud(2), Noeud(5))",listeATester.toString());
     }
 
     @Test
