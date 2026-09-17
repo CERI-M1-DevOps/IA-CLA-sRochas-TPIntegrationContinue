db4f0a2bbe73862641cfde42cd35a1018feed82c
ahrizmo <uapv2401091@e-mc129-16.ad.univ-avignon.fr>
2026-09-17 18:52:06 +0200

    tp1-exo3-2

--- src/test/java/liste/ListeSimpleTest.java
+++ src/test/java/liste/ListeSimpleTest.java
@@ -112,7 +112,7 @@ public class ListeSimpleTest {
         listeATester.ajout(4);
         listeATester.supprimePremier(5);
         assertEquals( "ListeSimple(Noeud(4), Noeud(3), Noeud(2), Noeud(1))",listeATester.toString());
-        assertEquals(3, listeATester.getSize());
+        assertEquals(4, listeATester.getSize());
     }
 
     @Test
